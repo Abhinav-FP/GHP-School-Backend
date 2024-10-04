96d02dae0fc864352e8a0852bf46464795350d88
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-10-04 12:39:32 +0530

    enquiry page done

--- src/index.js
+++ src/index.js
@@ -49,6 +49,8 @@ app.use("/home", require("./routes/homeRoutes"));
 app.use("/result", require("./routes/resultRoutes"));
 app.use("/fees", require("./routes/feesRoutes"));
 app.use("/facilities", require("./routes/facilitiesRoutes"));
+app.use("/inquiry", require("./routes/InquiryRoutes"));
+
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use("/donation", require("./routes/facilitiesRoutes"));
