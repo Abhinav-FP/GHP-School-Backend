4775c401b21b8d04c673b221bcd09ce710e05292
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-10-09 13:05:25 +0530

    manage all  new Api implenetion

--- src/controller/PaymentController.js
+++ src/controller/PaymentController.js
@@ -13,7 +13,7 @@ const razorpayInstance = new Razorpay({
     const { amount, currency = 'INR', receipt } = req.body; 
     try {
       const options = {
-        amount: amount * 100, 
+        amount: amount, 
         currency,
         receipt,
         payment_capture: 1, 
