ceb0f6b6bdad6475e0b7f5f919f786f2b9c55e6a
Abhinav-FP <a.mathur@futureprofilez.com>
2024-10-16 17:46:31 +0530

    Gallery

--- src/routes/facilitiesRoutes.js
+++ src/routes/facilitiesRoutes.js
@@ -5,7 +5,7 @@ const { admingallery, galleryGet, galleryGetCategory, galleryDeleteById, gallery
 router.get("/gallery/get", galleryGet);
 router.get("/admingallery", admingallery);
 router.get("/gallery/get/:type", galleryGetCategory);
-router.post("/gallery/delete/:id", verifyToken, galleryDeleteById);
+router.post("/gallery/delete", verifyToken, galleryDeleteById);
 router.post("/sports/add", verifyToken, sportsAdd);
 router.get("/sports/get", sportsGet);
 router.post("/sports/delete", verifyToken, sportsDelete);
