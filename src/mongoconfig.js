b85b5f57e9177eeab52a0087fba577e4ef6ff867
Abhinav-FP <a.mathur@futureprofilez.com>
2024-12-19 15:27:30 +0530

    Mongo change

--- src/mongoconfig.js
+++ src/mongoconfig.js
@@ -4,11 +4,14 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 mongoose.connect(process.env.DB_URL, {
-   serverSelectionTimeoutMS: 5000, 
-   autoIndex: false,               
-   maxPoolSize: 10,                
-   socketTimeoutMS: 45000,         
-   family: 4                       
+   useNewUrlParser: true,   
+    serverSelectionTimeoutMS: 5000,    
+    autoIndex: false, // Don't build indexes 
+    maxPoolSize: 10, // Maintain up to 10 socket connections
+    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    family: 4 // Use IPv4, skip trying IPv6
+                    
 })
 .then(() => {
    console.log('MongoDB connected successfully');
